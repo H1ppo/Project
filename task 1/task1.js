01d11ad56a3beff8e449749ad52d7e6021f7a3f4
Alexey <hippo.bro@gmail.com>
2019-04-04 17:45:37 +0500

    Первый коммит

---
+++ task 1/task1.js
@@ -0,0 +1,22 @@
+﻿let money = prompt('Ваш бюджет на месяц?', '0');
+let time = prompt('Введите дату в формате YYYY-MM-DD', "2000-01-01");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses:'',
+    optionalExpenses:'',
+    income:'',
+    savings: false
+};
+let quest = prompt('Введите обязательную статью расходов в этом месяце',"");
+let ans = prompt('Во сколько обойдется?','0');
+appData.expenses = quest + ' : ' + ans + '; ';
+quest = prompt('Введите еще обязательную статью расходов в этом месяце',"");
+ans = prompt('Во сколько обойдется?','0');
+appData.expenses = appData.expenses + quest + ' : ' + ans;
+let oneday = 'Ваш бюджет на день: ' + money/30; 
+alert(oneday);
+
+
+
+
